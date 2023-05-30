adf5bcdfc059f87339c76614f61609dc541dac3a
DaiTangoMne <mksim3uchnev@gmail.com>
2023-05-30 15:52:23 +0300

    py connected

---
+++ py/main.py
@@ -0,0 +1,6 @@
+import sys
+
+if __name__ == '__main__':
+    sys.path.extend(['C:\\Users\\mksim\\Desktop\\ictis\\workloadICTIS\\py\\venv',
+                     'C:\\Users\\mksim\\Desktop\\ictis\\workloadICTIS\\py\\venv\\Lib\\site-packages'])
+    import pandas
